ca24de139648917d136a600c499171acd2999e6a
Rodrigo Estape de Oliveira <rodrigo_estape@hotmail.com>
2024-04-25 23:36:23 -0300

    feat: Initial config files

---
+++ src/main/java/com/econeigigobhoood/sgb/SIstemaDeGestaoDeBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.econeigigobhoood.sgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SIstemaDeGestaoDeBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SIstemaDeGestaoDeBibliotecaApplication.class, args);
+	}
+
+}
