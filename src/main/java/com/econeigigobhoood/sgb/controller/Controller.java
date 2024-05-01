358748be7953d1f11b8b0432c6ecbf275d8f7987
Rodrigo Ryudi Knaoyadani <100811087+Kanoyadani@users.noreply.github.com>
2024-05-01 18:21:04 -0300

    Update Controller.java
    Adicionado o Controller

--- src/main/java/com/econeigigobhoood/sgb/controller/Controller.java
+++ src/main/java/com/econeigigobhoood/sgb/controller/Controller.java
@@ -1,5 +1,66 @@
-package com.econeigigobhoood.sgb.controller;
+package com.econeigigobhoood.sgb.Model;
 
-public class Controller {
+public class Livro   {
+    
+    private static int Idlivro;
+    private static String Nome;
+    private static String Autor;
+    private static int Paginas;
+    private static String Status;
+    
+
+    public Livro(int Idlivro, String Nome, String Autor, int Paginas,String Status) {
+        Livro.Idlivro = Idlivro;
+        Livro.Nome = Nome;
+        Livro.Autor = Autor;
+        Livro.Paginas = Paginas;
+        Livro.Status = Status;
+    }
+
+    // Getters
+    public static int getIdlivro() {
+        return Idlivro;
+    }
+
+    public static String getNome() {
+        return Nome;
+    }
+
+    public static String getAutor() {
+        return Autor;
+    }
+
+    public static int getPaginas() {
+        return Paginas;
+    }
+
+    public static String getStatus() {
+        return Status;
+    }
+
+    // Setters
+    public static  int setIdlivro(int Idlivro) {
+        return Livro .Idlivro = Idlivro;
+    }
+
+    public static String setNome(String Nome) {
+        return Livro.Nome = Nome;
+    }
+
+    public static String setAutor(String Autor) {
+        return Livro.Autor = Autor;
+    }
+
+
+    public static int setPaginas(int Paginas) {
+        return Livro.Paginas = Paginas;
+    }
+
+    public static String setStatus(String Status) {
+        return Livro.Status = Status;
+    }
+
+   
+
     
 }
